86f275339c0dde2dbf551d18e9ef0d63c72b2592
Jeonyunjae <55024678+Jeonyunjae@users.noreply.github.com>
2021-09-16 01:44:49 +0900

    4.14 File Upload part One

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
-require("dotenv").config();
 import { ApolloServer } from "apollo-server";
-import schema from "./schema";
-import { getUser } from "./users/user.utils";
+import { typeDefs, resolvers } from "./schema";
+import { getUser } from "./users/users.utils";
 
+const PORT = process.env.PORT;
 const server = new ApolloServer({
-  schema,
-  context: async({ req }) => {
+  resolvers,
+  typeDefs,
+  context: async ({ req }) => {
     return {
       loggedInUser: await getUser(req.headers.token),
     };
   },
 });
-
-const PORT = process.env.PORT;
-
 server
   .listen(PORT)
   .then(() =>
